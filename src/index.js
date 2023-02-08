b406c18bb273f2bd392a3e4d36292a8dccd4efb6
Promit-revar <promit.revar2211@gmail.com>
2023-02-08 12:41:10 +0530

    Feat: Add jwt token generation and login verification

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
 //create an express server 
 const express = require('express');
 const router = require('./routes/userRoutes');
-
+const authRouter = require('./routes/loginRoutes');
+require('dotenv').config();
 //add swagger documentation
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('../swagger.json');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8000;
 
 // middlewares
 app.use(express.json());
 app.use('/api', router);
+app.use('/auth', authRouter);
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 
@@ -21,7 +23,7 @@ app.get('/', (req, res) => {
 
 
 
-//start a server on port 3000
+//start a server on port 8000
 app.listen(PORT, () => {
   console.log(`Server is up on port ${PORT}`);
 });
